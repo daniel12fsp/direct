0f3fb007cfdfa07513178efac8d5a6df78c9cd28
Daniel Pereira <daniel12fsp@yahoo.com.br>
2018-05-12 20:04:48 -0400

    Add xdescribe

--- src/inject.js
+++ src/inject.js
@@ -1,40 +1,40 @@
 const describe = (title, fn) => {
-	console.log(title);
-	fn();
+  console.log(title);
+  fn();
 };
 
 let $beforeEach;
 
-const beforeEach = (fn) => {
-	$beforeEach = fn;
+const beforeEach = fn => {
+  $beforeEach = fn;
 };
 const it = (title, fn) => {
-	console.log('beforeEach');
-	$beforeEach();
-	console.log('fn');
-	fn();
+  console.log("beforeEach");
+  $beforeEach && $beforeEach();
+  console.log("fn");
+  fn();
 };
 const xit = () => {};
-const expect = (valueReceped) => {
-	return {
-		toBe(valueExpected) {
-			if (valueExpected === valueReceped){
-                console.log("True");
-            } else {
-                throw Error("Value different");
-            }
-        },
-        toThrowError(Message){
-            try {
-                valueReceped();
-                throw Error("Doesnt have error");
-            } catch (error) {
-                console.log("True")
-            }
-        },
-        toEqual(valueExpected){
-            valueReceped.sort().toString() == valueExpected.sort().toString() 
-
-        }
-	};
+const xdescribe = () => {};
+const expect = valueReceped => {
+  return {
+    toBe(valueExpected) {
+      if (valueExpected === valueReceped) {
+        console.log("True");
+      } else {
+        throw Error("Value different");
+      }
+    },
+    toThrowError(Message) {
+      try {
+        valueReceped();
+        throw Error("Doesnt have error");
+      } catch (error) {
+        console.log("True");
+      }
+    },
+    toEqual(valueExpected) {
+      valueReceped.sort().toString() == valueExpected.sort().toString();
+    }
+  };
 };
